cfc9bf30684fc94d57e7c4e95181626565e0700e
Yu Sakita <54483936+sakitayu@users.noreply.github.com>
2019-09-30 18:17:29 +0900

    Delete test6.rb

--- test6.rb
+++
@@ -1,7 +0,0 @@
-@juice = [{price: 120, name: "cola", stock: 1}, # stockの値を変動させて実験
-          {price: 100, name: "water", stock: 0}, # stockの値を変動させて実験
-          {price: 200, name: "redbull", stock: 1}] # stockの値を変動させて実験
-
-@juice[0][:stock] += 1
-puts @juice[0][:stock]
-puts @juice
