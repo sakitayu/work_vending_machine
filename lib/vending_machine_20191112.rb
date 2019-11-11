52d45735a1f84f665e6b4842dc6fd2e48a392e22
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-12 03:08:14 +0900

    不要箇所削除

--- lib/vending_machine_20191112.rb
+++ lib/vending_machine_20191112.rb
@@ -11,15 +11,10 @@ class Drink
   
   def drink_stock(vm,number)
     @stock += number
-    self.item_listing(vm)
-  end
-  
-  def drink_info()
-    @info = {name: @name,price: @price,stock: @stock}
+    item_listing(vm)
   end
   
   def item_listing(vm)
-    drink_info()
     vm.item_lists.map { |key,value| key[:stock] = self.stock  if key[:name] == self.name}
   end
 end
@@ -41,7 +36,6 @@ end
 class VendingMachine
   attr_reader :slot_money,:sales,:item_lists,:id
   MONEY = [10, 50, 100, 500, 1000].freeze
-  
   def initialize(id)
     @id = id
     @slot_money = 0
@@ -81,41 +75,41 @@ class VendingMachine
   end
   
   def possible_to_buy_lists
-    vm_id = Array[id: self.id]
     lists = self.item_lists.find_all {|key,value| (key[:price] <= self.slot_money) && (key[:stock] > 0)}
+    vm_id = Array[id: self.id]
     vm_id.push(lists)
   end
 end
-# 客をインスタンス化
-guest = Guest.new
-# 引数は自販機のIDとなる
-vm1 = VendingMachine.new(1)
-# その自販機が扱う商品を定義
-vm1_cola = Drink.new(vm1,"cola",120)
-vm1_water = Drink.new(vm1,"water",100)
-vm1_redbull = Drink.new(vm1,"redbull",200)
-# その自販機が扱う商品を数量分ストック
-vm1_cola.drink_stock(vm1,5)
-vm1_water.drink_stock(vm1,5)
-vm1_redbull.drink_stock(vm1,5)
-# その自販機にお金を入れる
-vm1.slot(1000)
-# その客がその自販機の商品の購入ボタンを押す
-guest.buy(vm1,vm1_cola)
-vm1.slot(1000)
-guest.buy(vm1,vm1_cola)
-vm1.slot(1000)
-guest.buy(vm1,vm1_cola)
-vm1.slot(1000)
-guest.buy(vm1,vm1_cola)
-vm1.slot(1000)
-guest.buy(vm1,vm1_cola)
-vm1.slot(1000)
-guest.buy(vm1,vm1_cola)
-vm1.slot(1000)
-guest.buy(vm1,vm1_cola)
-vm1.slot(1000)
-guest.buy(vm1,vm1_water)
-vm1.slot(1000)
-# 購入できる商品リストを返す
-p vm1.possible_to_buy_lists
+# # 客をインスタンス化
+# guest = Guest.new
+# # 引数は自販機のIDとなる
+# vm1 = VendingMachine.new(1)
+# # その自販機が扱う商品を定義
+# vm1_cola = Drink.new(vm1,"cola",120)
+# vm1_water = Drink.new(vm1,"water",100)
+# vm1_redbull = Drink.new(vm1,"redbull",200)
+# # その自販機が扱う商品を数量分ストック
+# vm1_cola.drink_stock(vm1,5)
+# vm1_water.drink_stock(vm1,5)
+# vm1_redbull.drink_stock(vm1,5)
+# # その自販機にお金を入れる
+# vm1.slot(1000)
+# # その客がその自販機の商品の購入ボタンを押す
+# guest.buy(vm1,vm1_cola)
+# vm1.slot(1000)
+# guest.buy(vm1,vm1_cola)
+# vm1.slot(1000)
+# guest.buy(vm1,vm1_cola)
+# vm1.slot(1000)
+# guest.buy(vm1,vm1_cola)
+# vm1.slot(1000)
+# guest.buy(vm1,vm1_cola)
+# vm1.slot(1000)
+# guest.buy(vm1,vm1_cola)
+# vm1.slot(1000)
+# guest.buy(vm1,vm1_cola)
+# vm1.slot(1000)
+# guest.buy(vm1,vm1_water)
+# vm1.slot(1000)
+# # 購入できる商品リストを返す
+# p vm1.possible_to_buy_lists
